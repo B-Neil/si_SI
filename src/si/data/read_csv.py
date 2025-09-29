a2f8643865eb838219353da22f0df22c58c6a95c
bneil <joaoferreira.ac23@gmail.com>
2025-09-29 14:00:42 +0100

    up

---
+++ src/si/data/read_csv.py
@@ -0,0 +1,41 @@
+from si_SI.src.si.data.dataset import Dataset
+import pandas as pd
+import numpy as np
+
+def read_csv(filename: str, sep: str, features: bool = False, label : bool = False) -> Dataset:
+    """
+    Reads a CSV file and returns a Dataset object
+    Parameters
+    ----------
+    filename : str
+        The path to the CSV file
+    sep : str, optional
+        The separator used in the CSV file, by default ","
+    label : bool, optional
+        Whether the CSV file has a label column, by default True
+    Returns
+    -------
+    Dataset
+        The Dataset object containing the data from the CSV file
+    """
+    pass
+
+    df = pd.read_csv(filename, sep=sep)
+
+    if features and label:
+        x = df.iloc[:,:-1].to_numpy()
+        y = df.iloc[:,-1].to_numpy()
+        feature_names = df.columns = df.columns[:-1]
+        label_name = df.columns[-1]
+        return Dataset(X=x, y=y, features=feature_names, label=label_name)
+    elif features and not label:
+        x = df.to_numpy()
+        feature_names = df.columns
+        return Dataset(X=x, features=feature_names)
+    elif label:
+        x = np.array()
+        y = df.iloc[:,-1].to_numpy()
+        label_name = df.columns[-1]
+        return Dataset(X=x, y=y, label=label_name)
+    else:
+        return None
---
+++ src/si/data/write_csv.py
@@ -0,0 +1,40 @@
+from si_SI.src.si.data.dataset import Dataset
+import pandas as pd
+import numpy as np
+
+def write_csv(filename: str, dataset: Dataset, sep: str = ",", features: bool = False, label: bool = False) -> None:
+    """
+    Writes a Dataset object to a CSV file
+
+    Parameters
+    ----------
+    filename : str
+        The path to the CSV file
+    dataset : Dataset
+        The Dataset object containing the data to be written
+    sep : str, optional
+        The separator used in the CSV file, by default ","
+    features : bool, optional
+        Whether to include the feature columns in the CSV file, by default none
+    label : bool, optional
+        Whether to include the label column in the CSV file, by default none
+
+    Returns
+    -------
+    None
+    """
+    df = pd.DataFrame(dataset.X, columns=dataset.features)
+
+    if features:
+        df.columns = dataset.features
+    
+    if label:
+        y = df.y = dataset.y
+        label_name = dataset.label
+
+    else:
+        y= None
+        label_name = None
+    
+
+    df.to_csv(filename, sep=sep, index=False)
